133c0c6658da6b4eb6d162945c138785966b79a8
Abhishek Bhardwaj <122768913+abhibharog@users.noreply.github.com>
2024-11-23 11:56:17 +0530

    Add files via upload

---
+++ 2210990044_2test2/2210990044_test2/MeaalDetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MeaalDetailsViewController.swift
+//  2210990044_test2
+//
+//  Created by student-2 on 23/11/24.
+//
+
+import Foundation
+import UIKit
+
+struct Meal1 {
+    var name : String
+    var calories : Int
+    var preparationTime : String
+    var category : String
+    var ingredients : String
+    var instructions : String
+    var thumbnail : UIImage?
+}
---
+++ 2210990044_2test2/2210990044_test2/SummaryViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SummaryViewController.swift
+//  2210990044_test2
+//
+//  Created by student-2 on 23/11/24.
+//
+
+import UIKit
+
+class SummaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        super.viewDidLoad()
+    }
+    
+    @IBAction func dismissModal(_ sender: UIButton) {
+        dismiss(animated:true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
